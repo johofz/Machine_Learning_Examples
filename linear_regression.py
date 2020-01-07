e7a3aa5a8f4751ebfd53d3253f3f8a06f59b522b
Johannes Höfler <johannes.hoefler@outlook.com>
2020-01-07 12:24:28 +0100

    restructuring of some lines

--- linear_regression.py
+++ linear_regression.py
@@ -10,12 +10,9 @@ from matplotlib import style
 style.use('ggplot')
 
 df = quandl.get("EOD/AAPL", authtoken=api_key)
-print(df.head())
-
 df = df[['Adj_Open', 'Adj_High', 'Adj_Low', 'Adj_Close', 'Adj_Volume',]]
 df['HL_PCT'] = (df['Adj_High'] - df['Adj_Low']) / df['Adj_Low'] * 100.0
 df['PCT_CHANGE'] = (df['Adj_Close'] - df['Adj_Open']) / df['Adj_Open'] * 100.0
-
 df = df[['Adj_Close', 'HL_PCT',  'PCT_CHANGE', 'Adj_Volume']]
 
 forecast_col = 'Adj_Close'
@@ -35,7 +32,6 @@ y = np.array(df['label'])
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 algorithm = 'linear'
-
 if algorithm == 'svm':
     clf = svm.SVR()
 elif algorithm == 'linear':
@@ -45,9 +41,6 @@ clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
 forecast_set = clf.predict(X_lately)
-    
-
-print(forecast_set, accuracy, forecast_out)
 
 df['Forecast'] = np.nan
 
@@ -60,6 +53,10 @@ for i in forecast_set:
     next_date = datetime.datetime.fromtimestamp(next_unix)
     next_unix += one_day
     df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+print(accuracy)
+print(df.head())
+print(df.tail())
 
 df['Adj_Close'].plot()
 df['Forecast'].plot()
